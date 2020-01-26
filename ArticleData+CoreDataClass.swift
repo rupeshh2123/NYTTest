1349e6d271d8fd387025c5aa4317d38469258eab
rupesh jaiswal <rupeshh2123@gmail.com>
2020-01-26 13:08:38 +0530

    - Created coredata model

---
+++ ArticleData+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleData+CoreDataClass.swift
+//  
+//
+//  Created by Rupesh Jaiswal on 26/01/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ArticleData)
+public class ArticleData: NSManagedObject {
+
+}
